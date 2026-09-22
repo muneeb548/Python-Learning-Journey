a34dc2525f4f9d24a45b9baef50f5ea0045e4a15
Raja Muneeb Ahmed <123970597+muneeb548@users.noreply.github.com>
2026-09-22 11:25:24 +0500

    Add Student Information Task

---
+++ Day-05/student_information.py
@@ -0,0 +1,19 @@
+# In this task, I practiced:
+# 1. Creating variables
+# 2. Using different data types: string, integer, float and boolean
+# 3. Using variables with print()
+# 4. Using comments
+# 5. Using the \n escape sequence
+# 5. Displaying student information
+name = "Muneeb"
+age = 23
+cgpa = 2.88
+is_student = True
+city = "Hassan Abdal"
+
+print("=== Student Information ===\n")
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student:", is_student)
+print("City:", city)
