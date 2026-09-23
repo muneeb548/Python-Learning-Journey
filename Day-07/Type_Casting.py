d18ea246bc7b1e9922ed7338eb255fd873d9e61c
Raja Muneeb Ahmed <123970597+muneeb548@users.noreply.github.com>
2026-09-23 11:28:25 +0500

    Type_Casting Program

---
+++ Day-07/Type_Casting.py
@@ -0,0 +1,20 @@
+#Example of TypeCasting
+a = "1"
+#a=1
+b = "3"
+#b=3
+print(int(a)+int(b))
+#Example of Explicit Type_Casting
+string = "15"
+number = 7
+string_number = int(string)
+sum = number + string_number
+print("The sum of both the number is ",sum)
+#Example of Implict TypeCasting
+c=7
+print(type(c))
+d=8.0
+print(type(d))
+result=c+d
+print(result)
+print(type(result))
