b05bcd9e11a73cb755be0519e4e4cd075c2cc1f3
Raja Muneeb Ahmed <123970597+muneeb548@users.noreply.github.com>
2026-09-23 12:04:20 +0500

    Taking user Input in Python

---
+++ Day-08/User_input.py
@@ -0,0 +1,14 @@
+# In this task, I practiced:
+# 1. Taking user input using input()
+# 2. Storing user input in variables
+# 3. Printing user input using print()
+# 4. Understanding that input() returns data as a string
+# 5. Converting string input into integer using int()
+# 6. Performing addition after converting input into integers
+variable =(input("My name is "))
+print("My name is ",variable)
+
+x = (input("Enter First Number"))
+y = (input("Enter second Number"))
+print(x+y)
+print(int(x)+int(y))
