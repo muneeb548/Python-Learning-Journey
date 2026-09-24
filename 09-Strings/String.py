8e08977bff45217c68b518512aad4105693a6fd4
Raja Muneeb Ahmed <123970597+muneeb548@users.noreply.github.com>
2026-09-24 11:22:09 +0500

    String Practice

---
+++ 09-Strings/String.py
@@ -0,0 +1,21 @@
+# Creating strings using double quotes
+name = "raja"
+# Creating another string
+friend = "bila"
+# Using double quotes inside a string
+Apple = 'He said "I want to eat an apple".'
+# Printing the string
+print(Apple)
+# Multiline String Example
+bila = '''Hi Raja, kya
+haal hai tumhara?'''
+# Printing the multiline string
+print(bila)
+# Accessing characters of a string using index
+# String index starts from 0
+print(name[0])
+print(name[1])
+# Looping through a string character by character
+print("Let's use a for loop\n")
+for character in name:
+    print(character)
